f65b206977cb3a51e5b09f58ea6cb9c7f85b0873
Cynthia Bell McGillis <12412365+cynthiabellmcgillis@users.noreply.github.com>
2022-05-06 17:50:34 +0000

    So much.

---
+++ pages/components/ImageGrid.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {motion, MotionConfig} from "framer-motion";
+
+const images = ['imagetest.png'];
+
+
+
+function ImageGrid() {
+  
+  return (
+    <div className="img-grid">
+      {images &&
+        images.map((image,i) => (
+          <motion.div
+            layout
+            whileHover={{opacity: 1}}
+            className="img-wrap"
+            key={i}
+          >
+            <motion.img
+              src={`images/${image}`}
+              alt="uploadded pic"
+              initial={{opacity: 0}}
+              animate={{opacity: 1}}
+              transition={{delay: 1}}
+            />
+          </motion.div>
+        ))}
+    </div>
+  );
+}
+
+export default ImageGrid;
